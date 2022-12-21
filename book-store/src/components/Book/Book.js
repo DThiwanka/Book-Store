8b803bf14135cc116f1c2a23fd773b7144276d16
Dulaj Thiwanka <86193439+DThiwanka@users.noreply.github.com>
2022-12-22 01:32:09 +0530

    Delete Component

--- book-store/src/components/Book/Book.js
+++ book-store/src/components/Book/Book.js
@@ -1,11 +1,24 @@
-import {Button} from "@mui/material";
+import { Button } from "@mui/material";
 import React from 'react';
-import { Link } from "react-router-dom";
+import axios from "axios";
+import { Link, useNavigate } from "react-router-dom";
 import "./Book.css";
 
 
 const Book = (props) => {
+    const history = useNavigate();
+
     const { _id, name, author, description, price, available, image } = props.book;
+
+    const deleteHandler = async() => {
+
+
+        await axios.delete(`http://localhost:5000/books/${_id}`)
+            .then((res) => res.data)
+            .then(() => history("/"))
+            .then(() => history("/books"));
+    }
+
     return (
         <div className="card">
             <img src={image} alt={name} />
@@ -18,8 +31,8 @@ const Book = (props) => {
             <h3>Price: {price}</h3>
             {/* <p>Availability: {available}</p> */}
 
-            <Button LinkComponent={Link} to={`/books/${_id}`} sx={{mt:'auto'}}>Update</Button>&nbsp;
-            <Button sx={{mt:'auto'}}>Delete</Button>
+            <Button LinkComponent={Link} to={`/books/${_id}`} sx={{ mt: 'auto' }}>Update</Button>&nbsp;
+            <Button onClick={deleteHandler} sx={{ mt: 'auto' }}>Delete</Button>
         </div>
     )
 }
